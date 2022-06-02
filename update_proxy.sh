f9cf8bd0f8b26bb09c1fee0f6b97ab73f126b824
JeremyWaltenspuehl <101863751+JeremyWaltenspuehl@users.noreply.github.com>
2022-06-02 17:19:05 +0200

    Update update_proxy.sh

--- update_proxy.sh
+++ update_proxy.sh
@@ -1,6 +1,5 @@
 rpm -Uvh https://repo.zabbix.com/zabbix/6.0/rhel/8/x86_64/zabbix-release-6.0-5.el8.noarch.rpm
 systemctl stop zabbix-proxy.service
-
 rm -y /var/lib/sqlite/zabbix_proxy.db
 dnf update -y
 systemctl start zabbix-proxy.service
