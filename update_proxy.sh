c2c04849d0dd1bdec5055e68321c5cad88705cf6
JeremyWaltenspuehl <101863751+JeremyWaltenspuehl@users.noreply.github.com>
2022-06-02 17:17:46 +0200

    Add files via upload

--- update_proxy.sh
+++ update_proxy.sh
@@ -1,6 +1,7 @@
 rpm -Uvh https://repo.zabbix.com/zabbix/6.0/rhel/8/x86_64/zabbix-release-6.0-5.el8.noarch.rpm
-dnf clean all
-systemctl stop zabbix-proxy
+systemctl stop zabbix-proxy.service
+
 rm -y /var/lib/sqlite/zabbix_proxy.db
-dnf update
-systemctl start zabbix-proxy
+dnf update -y
+systemctl start zabbix-proxy.service
+
